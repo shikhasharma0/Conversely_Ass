d26bf3417ba62ec52669e2c9172e2d28a5081597
Shikha Sharma <shikha0252@gmail.com>
2024-11-24 15:28:43 +0530

    Initial commit Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [matrix, setMatrix] = useState(Array(9).fill(null)); 
+  const [clickOrder, setClickOrder] = useState([]); 
+
+  const handleBoxClick = (index) => {
+    if (clickOrder.length === 9) return; 
+
+    if (matrix[index] === 'green') return;
+
+    const updatedMatrix = [...matrix];
+    updatedMatrix[index] = 'green';
+    setMatrix(updatedMatrix);
+    setClickOrder([...clickOrder, index]);
+
+    // setClickOrder here ....
+
+    if (clickOrder.length === 8) {
+      setTimeout(() => {
+        changeToOrange();
+      }, 300); 
+    }
+  };
+
+  const changeToOrange = () => {
+    let updatedMatrix = [...matrix];
+    // let update = {}
+    clickOrder.forEach((boxIndex, i) => {
+      setTimeout(() => {
+        updatedMatrix[boxIndex] = 'orange';
+        setMatrix([...updatedMatrix]); 
+      }, i * 300); 
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="matrix">
+      {matrix.map((color, index) => (
+        <div
+          key={index}
+          className="box"
+          style={{ backgroundColor: color || 'white' }}
+          onClick={() => handleBoxClick(index)}
+        ></div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
